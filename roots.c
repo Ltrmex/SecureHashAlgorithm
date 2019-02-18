fe2a70263f50212380f8a615e8b892a407afa4d9
Maciej <maciekmajchrzak23@wp.pl>
2019-02-18 18:13:07 +0000

    calculating sha constants

--- roots.c
+++ roots.c
@@ -1,4 +1,10 @@
+// Author: Maciej Majchrzak
+// Student Number: G00332746
+// References: https://web.microsoftstream.com/video/e5e7f5d8-3d2f-4115-9eaa-d9b1d6a91c4e
+
 #include <stdio.h>
+#include <math.h>
+#include <stdint.h> // 32 bit int to store
 
 unsigned int primes[] = {
 	    2,   3,   5,   7,  11,  13,  17,  19,  23,  29 
@@ -11,8 +17,19 @@ unsigned int primes[] = {
 };
 
 int main(int argc, char *argv[]){
+	// Long doubles for precision (numbers won't get rounded)
+	long double cubeRoot;
+	long double fraction;
+	long double multiply;
+
 	for(int i = 0; i < 64; i++){
-		printf("%d\n", primes[i]);
+		cubeRoot = cbrtl( (long double) primes[i] ); // get prime cube roots
+		fraction = cubeRoot - floorl(cubeRoot);	// get rid of numbers before the decimal point
+
+		multiply = fraction * 0x100000000; // shift decimal place
+		multiply = floorl(multiply); // get rid of stuff after decimal point
+
+		printf("%3d -> %03.6LG -> %03.6LG -> %08x \n", primes[i], cubeRoot, fraction, (uint32_t ) multiply); // print
 	}
 
 	return 0;
