12157ca195bfa55370d5972af04da5b47bb25e57
Maciej <maciekmajchrzak23@wp.pl>
2019-02-22 14:26:11 +0000

    basic bit operations

--- bits.c
+++ bits.c
@@ -4,7 +4,16 @@
 int main(int argc, char *argv[]){
 	uint32_t a = 1;
 	uint32_t b = 2;
+	
+	printf(" a: %08x\n b: %08x\n", a, b);
 
+	printf(" &: %08x\n", (a&b));
+	printf(" |: %08x\n", (a|b));
+	printf(" ^: %08x\n", (a^b));
+	printf(" ~: %08x\n", (~a));
+	printf("<<: %08x\n", (a<<1));
+	printf(">>: %08x\n", (a>>1));
+	
 	return 0;
 
 }
