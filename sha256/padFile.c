a287c21b25b900031dd47df31373114589014c95
Ltrmex <maciekmajchrzak23@wp.pl>
2019-03-28 02:14:02 +0000

    reading file

--- sha256/padFile.c
+++ sha256/padFile.c
@@ -2,6 +2,13 @@
 #include <stdint.h>
 
 int main(int argc, char *argv[]) {
+    FILE* f;
 
+    f = fopen(argv[1], "r");
+
+    printf("%c\n", fread(&c, 1, 1, f));
+
+    fclose(f);
+    
 	return 0;
 }//main()
