212181b6204a44eb56a33e455117b3e793dd020b
Ltrmex <maciekmajchrzak23@wp.pl>
2019-03-29 16:43:02 +0000

    padding finished

--- sha256/padFile.c
+++ sha256/padFile.c
@@ -7,16 +7,20 @@ union msgblock {
     uint64_t s[8];
 };
 
+enum status {READ, PAD0, PAD1, FINISH};
+
 int main(int argc, char *argv[]) {
     union msgblock M;
 
     uint64_t nobits = 0;
     uint64_t nobytes;
+
+    enum status S = READ;
     
     FILE* f;
     f = fopen(argv[1], "r");
 
-    while(!feof(f)) {
+    while(S == READ) {
         nobytes = fread(M.e, 1, 64, f);
         printf("Read: %211u bytes\n", nobytes);
 	nobits = nobits + (nobytes * 8);
@@ -30,8 +34,30 @@ int main(int argc, char *argv[]) {
                 M.e[nobytes] = 0x00;
             }//while
             M.s[7] = nobits;
+            S = FINISH;
         }//if
+        else if (nobytes < 64) {
+            S = PAD0;
+            M.e[nobytes] = 0x80;
+            while (nobytes < 64) {
+                nobytes = nobytes + 1;
+                M.e[nobytes] = 0x00;
+            }//while
+        } //else if
+        else if(feof(f)) {
+            S = PAD1;              
+        }//else if
     }//while
+
+    if (S == PAD0 || S== PAD1) {
+        for (i = 0; i < 56; i++)
+            M.e[i] = 0x00;
+
+        M.s[7] = nobits;
+    }//if
+    
+    if (S == PAD1)
+        M.e[0] = 0x80; 
 
     fclose(f);
     
